472b27d5bb59457e4d4a9dd751d361547fe5ec0d
1nf1n1t3f1r3 <1nf1n1t3f1r3@gmail.com>
2026-03-23 16:54:28 +0100

    Added Flights Controller Added Flights/Index.HTML.ERB

---
+++ app/controllers/flights_controller.rb
@@ -0,0 +1,21 @@
+class FlightsController < ApplicationController
+  def index
+    @airports = Airport.all.order(:code)
+
+    # Basic search params: departure_airport_id, arrival_airport_id, date
+    @flights = Flight.all
+
+    if params[:departure_airport_id].present?
+      @flights = @flights.where(departure_airport_id: params[:departure_airport_id])
+    end
+
+    if params[:arrival_airport_id].present?
+      @flights = @flights.where(arrival_airport_id: params[:arrival_airport_id])
+    end
+
+    if params[:date].present?
+      date = Date.parse(params[:date]) rescue nil
+      @flights = @flights.where(departure_time: date.beginning_of_day..date.end_of_day) if date
+    end
+  end
+end
