36e30e92eb91f8caeb3696469a8205795c3c3a25
1nf1n1t3f1r3 <1nf1n1t3f1r3@gmail.com>
2026-03-23 16:36:19 +0100

    Added Associations

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,4 +1,7 @@
 class Flight < ApplicationRecord
-  belongs_to :departure_airport
-  belongs_to :arrival_airport
+  belongs_to :departure_airport, class_name: "Airport"
+  belongs_to :arrival_airport, class_name: "Airport"
+
+  validates :departure_time, presence: true
+  validates :duration, presence: true, numericality: { only_integer: true, greater_than: 0 }
 end
