0bb43abbe30f1cd5ca1711a2fc33e292a25a9615
1nf1n1t3f1r3 <1nf1n1t3f1r3@gmail.com>
2026-03-25 20:37:14 +0100

    Added Booking Model Added Passenger Model

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -2,6 +2,8 @@ class Flight < ApplicationRecord
   belongs_to :departure_airport, class_name: "Airport"
   belongs_to :arrival_airport, class_name: "Airport"
 
+  has_many :bookings, dependent: :destroy
+
   validates :departure_time, presence: true
   validates :duration, presence: true, numericality: { only_integer: true, greater_than: 0 }
 end
